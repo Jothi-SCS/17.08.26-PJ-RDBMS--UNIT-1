0b5f26f541d086e772dc8304603714ff5201f173
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-17 11:01:08 +0530

    Create solution.sql

---
+++ Program1/solution.sql
@@ -0,0 +1,25 @@
+-- ============================================================
+-- PROGRAM 1
+-- Create Department Table
+-- ============================================================
+
+-- Student Name:
+-- Register Number:
+
+-- Create a table named Department with the following fields:
+--
+-- DepartmentID   NUMBER(5)      PRIMARY KEY
+-- DepartmentName VARCHAR(20)
+-- HOD            VARCHAR(20)
+--
+-- Add appropriate constraints such as PRIMARY KEY,
+-- UNIQUE and NOT NULL.
+
+
+-- WRITE YOUR SQL STATEMENT BELOW
+
+
+
+-- Display the structure of Department table.
+
+
